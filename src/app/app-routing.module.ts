258b168775c2edaec587ac3ff0679df1e3428d92
ilias mamalis <ilias.mamalis@unify.com>
2018-12-12 10:07:11 +0200

    add entries to route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// @ ilias import component to see the route 
+import { UsersComponent } from './users/users.component';
+import { DetailsComponent } from './details/details.component';
+import { PostsComponent } from './posts/posts.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{
+  path: '',
+  component: UsersComponent
+},
+{
+  path: 'posts',
+  component: PostsComponent
+},
+{
+  path: 'details/:id',
+  component: DetailsComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+
+// @ilias UsersComponent it was  added automatically by using 'ng generate new component users'
 import { UsersComponent } from './users/users.component';
+import { PostsComponent } from './posts/posts.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    UsersComponent
+    UsersComponent,
+    PostsComponent,
+    DetailsComponent
   ],
   imports: [
     BrowserModule,
